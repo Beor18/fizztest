8ff79e280d899b6f1aaac0fcaa2bd1aab521fb43
Beor18 <fer.ragnar19@gmail.com>
2019-04-28 11:36:57 -0300

    componentes

--- src/components/MenuPrincipal/Menu.js
+++ src/components/MenuPrincipal/Menu.js
@@ -11,19 +11,39 @@ class Menu extends Component {
                 <Container className="contenedor-secundario-menu-principal">
                 <Row>
                     <Col xs={12} md={6} className="col-menu-principal-izquierda">
-                        <li className="item-menu-principal-izquierda">
-                            <a href="http://#">HOMBRE</a>
-                        </li>
-                        <li className="item-menu-principal-izquierda">
-                            <a href="http://#">MUJER</a>
-                        </li>
-                        <li className="item-menu-principal-izquierda">
-                            <a href="http://#">NIÑOS</a>
-                        </li>
+                    <ul id="nav">
+                    <li><a href="http://#">Hombre</a>
+                        <span id="s1"></span>
+                        <ul class="subs">
+                            <li className="primer-col-submenu"><h5 className="titulo-sub">CALZADO</h5>
+                                <ul>
+                                    <li><a href="http://#">Botines</a></li>
+                                    <li><a href="http://#">Zapatillas</a></li>
+                                    <li><a href="http://#">Botines con tapones</a></li>
+                                    <li><a href="http://#">Ojotas / chinelas </a></li>
+                                    <li><a href="http://#">Sandalia</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <div className="producto-submenu producto ejemplo">
+                                    <img src="http://remote.fizzmod.com/TyQVKjcj4SXjSEWv/frontend/images/004632000F88356_1.jpg" className="producto-imagen" alt=""/>
+                                    <div className="sin-oferta"></div>
+                                    <div className="margin-producto">CAMISETA DE FUTBOL ADIDAS RIVER PLATE AWAY</div>
+                                    <p className="precio-producto margin-producto">$ 300</p>
+                                    <p className="categoria-producto">Futbol</p>
+                                </div>
+                            </li>
+                        </ul>
+                    </li>
+                    <li><a href="http://#">Mujer</a></li>
+                    <li><a href="http://#">Niños</a></li>
+                 </ul>
                     </Col>
                     <Col xs={12} md={6} className="col-menu-principal-derecha">
                         <li className="item-menu-principal-derecha precio-menu">0 $ 0,00</li>
-                        <li className="item-menu-principal-derecha">MENU</li>
+                        <li className="item-menu-principal-derecha">
+                            <span className="itemCarrito"></span>
+                        </li>
                         <li className="item-menu-principal-derecha">Carrito</li>
                     </Col>
                 </Row>
